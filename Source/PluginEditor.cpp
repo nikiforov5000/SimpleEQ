0bb3933196f89e78e840fecb0287839acd5d9f47
Boris Nikiforov <nikiforov5000@gmail.com>
2022-03-13 14:52:11 +0300

    added value suffixes

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -83,10 +83,10 @@ void RotarySlidersWithLabels::paint(juce::Graphics& g) {
 
     auto sliderBounds = getSliderBounds();
 
-    g.setColour(Colours::red);
-    g.drawRect(getLocalBounds());
-    g.setColour(Colours::yellow);
-    g.drawRect(sliderBounds);
+    // g.setColour(Colours::red);
+    // g.drawRect(getLocalBounds());
+    // g.setColour(Colours::yellow);
+    // g.drawRect(sliderBounds);
 
     getLookAndFeel().drawRotarySlider(g,
                                       sliderBounds.getX(),
@@ -113,7 +113,36 @@ juce::Rectangle<int> RotarySlidersWithLabels::getSliderBounds() const {
 }
 
 juce::String RotarySlidersWithLabels::getDisplayString() const {
-    return juce::String(getValue());
+    if (auto* choiceParam = dynamic_cast<juce::AudioParameterChoice*>(param)) {
+        return choiceParam->getCurrentChoiceName();
+    }
+
+    juce::String str;
+    bool addK = false;
+
+    if (auto* floatParam = dynamic_cast<juce::AudioParameterFloat*>(param)) {
+        float val = getValue();
+
+        if (val > 999.f) {
+            val /= 1000.f;
+            addK = true;
+        }
+
+        str = juce::String(val, (addK ? 2 : 0));
+    }
+    else {
+        jassertfalse;
+    }
+
+    if (suffix.isNotEmpty()) {
+        str << " ";
+        if (addK) {
+            str << "k";
+        }
+        str << suffix;
+    }
+
+    return str;
 }
 
 //==============================================================================
